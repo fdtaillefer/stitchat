75f1bc0582624ebf7b6552e911fc7ee9a64214dd
Francois Taillefer <draconaur@gmail.com>
2013-06-10 16:30:12 -0400

    - Adding express, letting it take care of serving the javascript; also letting it take care of rendering (as well as compiling, I presume), the template. Most of the server's code became unneeded in one fell swoop so it's gone. - Adding consolidate to help express deal with dust templates.

--- server.js
+++ server.js
@@ -1,94 +1,20 @@
 var http = require('http');
+var express = require('express');
+var app = express();
 var dust = require('dustjs-linkedin');
-var url = require("url");
-var path = require("path");
-var fs = require('fs');
+var consolidate = require('consolidate');
 var socketio = require('socket.io');
 var port = require('./js/constants').port;
 
-var contentTypesByExtension = {
-    '.css':  "text/css",
-    '.js':   "text/javascript"
-};
-
-/**
- * Feeds the provided file to the provided http response, as the provided content type.
- * The file's existence must already have been verified.
- * @param response An http response
- * @param filename An absolute path to the file
- * @param contentType and http content type in String form, such as "text/javascript"
- */
-function returnFile(response, filename, contentType){
-    fs.readFile(filename, "binary", function(err, file) {
-        if(err) {
-            response.writeHead(500, {"Content-Type": "text/plain"});
-            response.write(err + "\n");
-            response.end();
-            return;
-        }
-
-        var headers = {};
-        headers["Content-Type"] = contentType;
-        response.writeHead(200, headers);
-        response.write(file, "binary");
-        response.end();
-    });
-}
-
-/**
- * Feeds the Stitchat main page to the provided http response.
- * @param response An http response
- */
-function returnChat(response){
-    fs.readFile('./js/templates.js', function(err, data){
-        var compiled = String(data);
-        console.log('Compiled template loaded.');
-        dust.loadSource(compiled);
-        dust.render('chatPage', {'title':'Stitchat'}, function(err, out){
-
-            console.log('Template rendered');
-
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(out);
-            response.end();
-        })
-    });
-}
-
-/**
- * Handles requests to the Stitchat server.
- * @param request An http request
- * @param response An http response
- */
-function onRequest(request, response) {
-    console.log('Request received.');
-
-    var uri = url.parse(request.url).pathname
-    var filename = path.join(process.cwd(), uri);
-
-    console.log('Request for filename ' + filename);
-
-    path.exists(filename, function(exists) {
-        if(!exists) {
-            response.writeHead(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-            return;
-        }
-
-        var contentType = contentTypesByExtension[path.extname(filename)];
-        if(contentType){
-            returnFile(response, filename, contentType);
-        } else {
-            returnChat(response);
-        }
-
-    });
-};
-
 function start(){
-    var server = http.createServer(onRequest).listen(port);
-    var io = socketio.listen(server);
+    var io = require('socket.io').listen(app.listen(port));
+    app.set('views', __dirname + '/tpl');
+    app.set('view engine', "dust");
+    app.engine('dust', consolidate.dust);
+    app.use(express.static(__dirname + '/js'));
+    app.get("/", function(req, res){
+        res.render("chatPage", {"title":"Stitchat"});
+    });
 
     //When a client connects, we want to...
     io.sockets.on('connection', function(socket){
