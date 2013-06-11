7d58ca4b7308d0dfd93d245f1f1a41ed6fb0470a
Francois Taillefer <draconaur@gmail.com>
2013-06-11 14:04:38 -0400

    - Renamed chat.js to chatPage.js - Added requireJS and eliminated most header script tags.

---
+++ js/chatPage.js
@@ -0,0 +1,51 @@
+var port = 8888;
+
+require.config({
+    "paths":{
+        "jquery":"//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min"
+    }
+});
+
+require(["jquery", "../socket.io/socket.io"], function(jQuery, io){
+
+    jQuery.noConflict();
+    jQuery(document).ready(function(){
+        function onMessage(data){
+            if(data.message){
+
+                //Remember if the chatField's scroll is at bottom
+                var scrollBottomed = chatDisplay.prop('scrollHeight') - chatDisplay.scrollTop() == chatDisplay.height();
+
+                //Append a div inside chatField
+                var newLine = jQuery('<div class="chatLine"></div>');
+                chatDisplay.append(newLine);
+                newLine.text(data.message);
+
+                //If scroll was at bottom, put it back to bottom
+                if(scrollBottomed){
+                    chatDisplay.scrollTop(chatDisplay.prop('scrollHeight') - chatDisplay.height())
+                }
+            } else {
+                console.log('Message with no message? : ', data);
+            }
+        }
+
+
+        var socket = io.connect('http://localhost:' + port);
+        var field = jQuery('#chatField');
+        var sendButton = jQuery('#sendButton');
+        var chatDisplay = jQuery('#chatDisplay');
+
+        socket.on('message', onMessage);
+
+        sendButton.on('click', function(event){
+            event.preventDefault();
+            event.stopPropagation();
+            var data = {};
+            data['message'] = field.val();
+            socket.emit('sendChat', data);
+            field.val('');
+        });
+
+    });
+});
