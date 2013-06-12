6d57712b0026d65dea48c749fdec0167849b59f9
Francois Taillefer <draconaur@gmail.com>
2013-06-12 12:54:49 -0400

    - Since templates are now rendered client-side, consolidate is no longer needed. Dust is still needed for the grunt task to be able to compile templates.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-var dust = require('dustjs-linkedin');
-var consolidate = require('consolidate');
 var socketio = require('socket.io');
 
 var requirejs = require('requirejs');
@@ -18,9 +16,6 @@ function start(){
     var io = require('socket.io').listen(app.listen(constants.CHAT_PORT));
 
     //Configure our app to render dust templates
-    app.set('views', __dirname + '/tpl');
-    app.set('view engine', "dust");
-    app.engine('dust', consolidate.dust);
     app.use(express.static(__dirname + '/js'));
 
     app.get("/", function(req, res){
