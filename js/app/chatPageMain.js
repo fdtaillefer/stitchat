8cb642f12224ca7c7a861c2c830840d840efa8df
Francois Taillefer <draconaur@gmail.com>
2013-06-13 13:41:17 -0400

    - Refactored chatPageMain.js, moving some code elsewhere (componentUtils) and putting functionality into cohesive functions.

--- js/app/chatPageMain.js
+++ js/app/chatPageMain.js
@@ -1,64 +1,78 @@
-require(["jquery", "socket.io", "app/pageBuilder", "app/constants"], function(jQuery, io, pageBuilder, constants){
+require(["jquery", "socket.io", "app/pageBuilder", "app/componentUtils", "app/constants"], function(jQuery, io, pageBuilder, componentUtils, constants){
+
+    /**
+     * Handles an incoming user message.
+     * @param Data object describing the message.
+     */
+    var onUserMessage = function(data){
+
+        //Append a div inside chatField
+        var newLine = jQuery('<div class="userMessage"></div>');
+        newLine.text(data.message);
+        componentUtils.appendMaintainingScroll(jQuery('#chatDisplay'), newLine);
+    }
+
+    /**
+     * Handles an incoming system message.
+     * @param Data object describing the message.
+     */
+    var onSystemMessage = function(data){
+
+        //Append a div inside chatField
+        var newLine = jQuery('<div class="systemMessage"></div>');
+        newLine.text(data.message);
+        componentUtils.appendMaintainingScroll(jQuery('#chatDisplay'), newLine);
+    }
+
+    /**
+     * Sends a chat message to the chat server.
+     * @param socket The socket that will send the message
+     * @param message The actual text of the message
+     */
+    function outputChatMessage(socket, message){
+
+        //Build message data
+        var data = {};
+        data['message'] = message;
+
+        //Send message
+        socket.emit('sendChat', data);
+    }
+
+    /**
+     * Performs UI tasks related to sending a chat message, as well as ouputting the message to the server.
+     * @param socket The socket that will send the message
+     */
+    function sendChatMessage(socket){
+        var chatField = jQuery('#chatField');
+
+        outputChatMessage(socket, chatField.val());
+
+        //Clear the field
+        chatField.val('');
+    }
+
+    //Once initial page is done loading, run client-side initialization
     jQuery(document).ready(function(){
 
-        /**
-         * Appends newNode to element, scrolling element back to bottom if it was previously that way.
-         */
-        function appendMaintainingScroll(element, newNode){
-
-            //Remember if the element's scroll is at bottom
-            var scrollBottomed = element.prop('scrollHeight') - element.scrollTop() == element.height();
-
-            //Append the new node inside element
-            element.append(newNode);
-
-            //If scroll was at bottom, put it back to bottom
-            if(scrollBottomed){
-                element.scrollTop(element.prop('scrollHeight') - element.height());
-            }
-        }
-
+        //Render template and apply the result to the skeleton.
+        //We have token data here (a static title) for now, to validate that rendering used it.
         pageBuilder.setTemplateToElement("chatPage", {"title":"Stitchat"}, jQuery("#pageContainer"));
 
-        var onUserMessage = function(data){
-
-            //Append a div inside chatField
-            var newLine = jQuery('<div class="userMessage"></div>');
-            newLine.text(data.message);
-            appendMaintainingScroll(chatDisplay, newLine);
-        }
-
-        var onSystemMessage = function(data){
-
-            //Append a div inside chatField
-            var newLine = jQuery('<div class="systemMessage"></div>');
-            newLine.text(data.message);
-            appendMaintainingScroll(chatDisplay, newLine);
-        }
-
-
-        var socket = io.connect( constants.HOST + ':' + constants.CHAT_PORT);
-        var field = jQuery('#chatField');
-        var sendButton = jQuery('#sendButton');
-        var chatDisplay = jQuery('#chatDisplay');
-
+        //Setup event handlers for chat events incoming from server
+        var chatConnectionString = constants.HOST + ':' + constants.CHAT_PORT;
+        var socket = io.connect(chatConnectionString);
         socket.on('userMessage', onUserMessage);
         socket.on('systemMessage', onSystemMessage);
 
-        sendButton.on('click', function(event){
+        //Setup event handlers for graphical components
+        jQuery('#sendButton').on('click', function(event){
 
             //Prevent the click from sending an http request
             event.preventDefault();
             event.stopPropagation();
 
-            //Send the message
-            var data = {};
-            data['message'] = field.val();
-            socket.emit('sendChat', data);
-
-            //Clear the field
-            field.val('');
+            sendChatMessage(socket)
         });
-
     });
 });
---
+++ js/app/componentUtils.js
@@ -0,0 +1,42 @@
+/**
+ * Returns whether element's vertical scroll is all the way at the bottom.
+ * @param element Element to check
+ * @return
+ */
+var isScrollAtBottom = function(element){
+    return element.prop('scrollHeight') - element.scrollTop() === element.height();
+}
+
+/**
+ * Sets element's vertical scroll all the way to the bottom.
+ * @param element Element to scroll down
+ */
+var scrollToBottom = function(element){
+    element.scrollTop(element.prop('scrollHeight') - element.height());
+}
+
+/**
+ * Appends newNode to element, scrolling element back to bottom if it was previously that way.
+ * @param element Element at the end of which newNode is appended
+ * @param newNode Element to append
+ */
+var appendMaintainingScroll = function(element, newNode){
+
+    //Remember if the element's scroll is at bottom
+    var scrollAtBottom = isScrollAtBottom(element);
+
+    //Append the new node inside element
+    element.append(newNode);
+
+    //If scroll was at bottom, put it back to bottom
+    if(scrollAtBottom){
+        scrollToBottom(element);
+    }
+}
+
+
+define({
+    "isScrollAtBottom": isScrollAtBottom,
+    "scrollToBottom": scrollToBottom,
+    "appendMaintainingScroll": appendMaintainingScroll
+});
